43171c810f14dfb5bc0b035cd2f38e7808475935
Luwin Wickramasinghe <luwin.anuka@gmail.com>
2024-11-07 16:20:59 +0530

    sign up form update

--- lib/Screens/Signup/components/signup_form.dart
+++ lib/Screens/Signup/components/signup_form.dart
@@ -15,31 +15,47 @@ class SignUpForm extends StatelessWidget {
       child: Column(
         children: [
           TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            textInputAction: TextInputAction.next,
+            textInputAction: TextInputAction.done,
             cursorColor: kPrimaryColor,
-            onSaved: (email) {},
             decoration: const InputDecoration(
-              hintText: "Your email",
+              hintText: "Your Full Name",
               prefixIcon: Padding(
                 padding: EdgeInsets.all(defaultPadding),
-                child: Icon(Icons.person),
+                child: Icon(Icons.lock),
               ),
             ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-            child: TextFormField(
-              textInputAction: TextInputAction.done,
-              obscureText: true,
-              cursorColor: kPrimaryColor,
-              decoration: const InputDecoration(
-                hintText: "Your password",
-                prefixIcon: Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                  child: Icon(Icons.lock),
+            child: Column(
+              children: [
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  cursorColor: kPrimaryColor,
+                  onSaved: (email) {},
+                  decoration: const InputDecoration(
+                    hintText: "Your email",
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(defaultPadding),
+                      child: Icon(Icons.person),
+                    ),
+                  ),
                 ),
-              ),
+                const SizedBox(height: defaultPadding),
+                TextFormField(
+                  textInputAction: TextInputAction.done,
+                  obscureText: true,
+                  cursorColor: kPrimaryColor,
+                  decoration: const InputDecoration(
+                    hintText: "Your password",
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(defaultPadding),
+                      child: Icon(Icons.lock),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           const SizedBox(height: defaultPadding / 2),
